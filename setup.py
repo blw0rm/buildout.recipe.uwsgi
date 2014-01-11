affb831858ce9e70f8f1220ad5478a36dadbfa62
Yaroslav D <blw0rm@gmail.com>
2014-01-11 20:04:47 +0200

    Made setup script compatible with python >= 3

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
+
+import sys
 from setuptools import setup, find_packages
+
+
+def get_text_from_file(fn):
+    text = open(fn, 'rb').read()
+    if sys.version_info >= (2, 6):
+        return text.decode('utf-8')
+    return text
 
 setup(
     name="buildout.recipe.uwsgi",
     version="0.0.20",
     description="Buildout recipe downloading, compiling and configuring uWSGI.",
-    long_description=open("README.rst", "r").read(),
+    long_description=get_text_from_file("README.rst"),
     author="Cosmin Lu\xc8\x9b\xc4\x83",
     author_email="q4break@gmail.com",
     license="BSD",
